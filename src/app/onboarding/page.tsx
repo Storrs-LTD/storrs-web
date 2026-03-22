d5804910cd0d60ff88c9fd67d9f97c9b338b7e72
chideracollins <86984699+chideracollins@users.noreply.github.com>
2026-03-22 21:11:15 +0100

    chore: Enable StorrsApp postMessage for testing purposes.

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -20,7 +20,7 @@ function OnboardingPageContent() {
   const storrsBusinessId = searchParams.get("storrs_business_id");
 
   useEffect(() => {
-    // window.StorrsApp?.postMessage(JSON.stringify({ hello: "hi" })); // testing
+    window.StorrsApp?.postMessage(JSON.stringify({ hello: "hi" })); // testing
     window.fbAsyncInit = function () {
       // SDK initialization
       window.FB.init({
